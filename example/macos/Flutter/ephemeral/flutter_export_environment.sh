2db694d03820cb456d36ebaf20f029d84bd93a6d
Hudson Afonso <hudson.afonso@gmail.com>
2025-05-26 09:57:39 -0300

    chore: Update Flutter SDK version and dependencies

--- example/macos/Flutter/ephemeral/flutter_export_environment.sh
+++ example/macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/hafonso/fvm/versions/3.24.2"
+export "FLUTTER_ROOT=/Users/hafonso/fvm/versions/3.32.0"
 export "FLUTTER_APPLICATION_PATH=/Users/hafonso/Workspace/dart/beta/flat_table/example"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
